6a98eb78cd9cbe36b1f07d3445f9998690de9bac
Mike Richardson <mike.richardson@exponentialent.com>
2020-10-16 13:51:54 -0700

    Improved Thumbnail lightbox so that the description for each image appears below it

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -2,84 +2,91 @@ import React from "react";
 import styled from "styled-components";
 import * as lightbox from "basiclightbox";
 
-const images = require.context('../img');
+const images = require.context("../img");
 
 const thumb = (props) => {
+	const createText = (text) => {
+		let txtDiv = document.createElement("div");
+		txtDiv.style.cssText =
+			"padding: 1px 25px 15px; background-color: #000102; text-align: left; color: #d8d8d8; font-family: sans-serif;";
 
-    const createTextDiv = text => {
-        let txtDiv = document.createElement("div");
-        txtDiv.style.cssText =
-            "display: flex; flex-direction: column; justify-content: center; align-items: stretch; flex: 1; padding: 0 2%; background-color: #000000ee; color: white; font-family: sans-serif;";
-        
-        if (text.header) {
-            let head = document.createElement("h2");
-            head.style.cssText = "text-align: center;";
-            head.textContent = text.header;
-            txtDiv.appendChild(head);
-        }
+		if (text.header) {
+			let head = document.createElement("h2");
+			head.style.cssText = "text-align: center;";
+			head.textContent = text.header;
+			txtDiv.appendChild(head);
+		}
 
-        if (text.paragraphs && text.paragraphs.length) {
-            text.paragraphs.forEach((par) => {
-                let parNode = document.createElement("p");
-                parNode.textContent = par;
-                txtDiv.appendChild(parNode);
-            });
-        }
+		if (text.paragraphs && text.paragraphs.length) {
+			text.paragraphs.forEach((par) => {
+				let parNode = document.createElement("p");
+				parNode.textContent = par;
+				txtDiv.appendChild(parNode);
+			});
+		}
 
-        if (text.gitHub) {
-            if (text.gitHub.url) {
-                let hlink = document.createElement("a");
-                hlink.style.cssText = "text-transform: none; color: #8db2e6";
-                hlink.href = text.gitHub.url;
-                hlink.target = "_blank";
-                hlink.textContent = text.gitHub.text || text.gitHub.url;
-                
-                // Prevent click on href from closing entire description
-                hlink.onclick = e => e.stopPropagation();
-                
-                let hlinkPara = document.createElement("p");
-                hlinkPara.appendChild(hlink);
-                txtDiv.appendChild(hlinkPara);
-            }
-        }
+		if (text.gitHub) {
+			if (text.gitHub.url) {
+				let hlink = document.createElement("a");
+				hlink.style.cssText = "text-transform: none; color: #8db2e6";
+				hlink.href = text.gitHub.url;
+				hlink.target = "_blank";
+				hlink.textContent = text.gitHub.text || text.gitHub.url;
 
-        let txtWrapperDiv = document.createElement("div");
-        txtWrapperDiv.style.cssText = "position: absolute; top: 0; right: 0; bottom: 0; left: 0; display: flex; justify-content: center; align-items: center; background-color: transparent; transition: opacity 0.5s";
-        txtWrapperDiv.appendChild(txtDiv);
+				// Prevent click on href from closing entire description
+				hlink.onclick = e => e.stopPropagation();
 
-        txtWrapperDiv.onclick = (e) => {
-            e.stopPropagation();
-            txtWrapperDiv.style.cssText += " opacity: 0; pointer-events: none";
-        };
+				let hlinkPara = document.createElement("p");
+				hlinkPara.appendChild(hlink);
+				txtDiv.appendChild(hlinkPara);
+			}
+		}
 
-        return txtWrapperDiv;
-    }
+		let txtWrapperDiv = document.createElement("div");
+		txtWrapperDiv.style.cssText = "background-color: transparent; transition: opacity 0.5s";
+		txtWrapperDiv.appendChild(txtDiv);
+
+		// txtWrapperDiv.onclick = (e) => {
+		//     e.stopPropagation();
+		//     txtWrapperDiv.style.cssText += " opacity: 0; pointer-events: none";
+		// };
+
+		return txtWrapperDiv;
+	};
 
 	const openLightbox = info => {
-		// lightbox.create(`<img src=${lSrc} alt="">`).show();
-
-        let mainDiv = document.createElement("div");
-		mainDiv.style.cssText = "position: relative";
+		let mainDiv = document.createElement("div");
+		mainDiv.style.cssText = "max-height: 100vh; overflow-y: auto; text-align: center;";
 
 		let mainImg = document.createElement("img");
-        mainImg.src = images(info.imgSrc);
-        
-        mainDiv.appendChild(mainImg);
+		mainImg.src = images(info.imgSrc);
+		mainImg.style.cssText = "max-width: 95vw; margin: 1vmin;";
+
+		mainDiv.appendChild(mainImg);
 
 		if (info.text) {
-			const txtDiv = createTextDiv(info.text)
-            mainDiv.appendChild(txtDiv);
-        }
-        
-        // Extra wrapper div required to get around the way basiclightbox handles events
-        let mainWrapperDiv = document.createElement("div");
-        mainWrapperDiv.appendChild(mainDiv);
+			const txtDiv = createText(info.text);
+			mainDiv.appendChild(txtDiv);
+		}
 
-		let lb = lightbox.create(mainWrapperDiv);
+		// Extra wrapper div required to get around the way basiclightbox handles events
+		let mainWrapperDiv = document.createElement("div");
+		mainWrapperDiv.appendChild(mainDiv);
+
+		let lb = lightbox.create(mainWrapperDiv, {
+			onShow: () => {
+				document.body.style.overflow = "hidden";
+			},
+			onClose: () => {
+				document.body.style.overflow = "auto";
+			},
+        });
+        
 		lb.show();
 
-		mainImg.onclick = () => { lb.close(); };
-        mainDiv.onclick = () => { lb.close(); }
+		mainDiv.onclick = () => {
+			lb.close();
+		};
 	};
 
 	return (
