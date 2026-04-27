23fa6904ab95f748a63baf598d2845eb84ce648e
Mike Richardson <contact@mikeswork.info>
2026-04-27 14:29:25 -0700

    cleanup vimeo embed

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -4,62 +4,53 @@ import Section from "./Section";
 import pageData from "../data/pageData.json";
 
 const vProps = {
-    w: 640,
-    h: 360
-}
+  w: 640,
+  h: 360,
+};
 
 const VidWrapper = styled.div`
-	text-align: center;
+  text-align: center;
 `;
 
 const Vid = styled.div`
-	margin: 0 0 40px;
+  margin: 0 0 40px;
 
-	iframe {
-		width: 90vw;
-		max-width: ${`${vProps.w}px`};
-        max-height: 45vw;
-        ${props => props.$vimeo && `max-height: 51vw;`}
-	}
+  iframe {
+    width: 90vw;
+    max-width: ${`${vProps.w}px`};
+    max-height: 45vw;
+  }
 `;
 
 const VideoContent = (props) => {
-	const videos = pageData.pages.video.content || [];
-	let vidId = 0;
+  const videos = pageData.pages.video.content || [];
+  let vidId = 0;
 
-	return (
-		<VidWrapper>
-			{videos.map((vidData) => {
-				const isVimeo = vidData.src.indexOf("youtube") === -1;
-
-				return (
-					<Vid key={vidId++} $vimeo={isVimeo}>
-						<iframe
-							title={vidData.title}
-							src={isVimeo ? `${vidData.src}?transparent=0` : vidData.src}
-							width={vProps.w}
-							height={vProps.h}
-							frameBorder="0"
-							allow={
-								isVimeo
-									? "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-									: "autoplay; fullscreen"
-							}
-							allowFullScreen
-						></iframe>
-					</Vid>
-				);
-			})}
-		</VidWrapper>
-	);
+  return (
+    <VidWrapper>
+      {videos.map((vidData) => (
+        <Vid key={vidId++}>
+          <iframe
+            title={vidData.title}
+            src={vidData.src}
+            width={vProps.w}
+            height={vProps.h}
+            frameBorder="0"
+            allow="autoplay; fullscreen"
+            allowFullScreen
+          ></iframe>
+        </Vid>
+      ))}
+    </VidWrapper>
+  );
 };
 
 const VideoSection = (props) => {
-	return (
-		<Section path={pageData.pages.video.path} title="Video (just for fun)">
-			<VideoContent />
-		</Section>
-	);
+  return (
+    <Section path={pageData.pages.video.path} title="Video (just for fun)">
+      <VideoContent />
+    </Section>
+  );
 };
 
 export default VideoSection;
